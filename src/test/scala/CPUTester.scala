d60fb532148791c5995519bfa979c294a330779e
SaltyfishAlpha <saltyfishalpha@gmail.com>
2023-06-20 15:15:16 +0800

    fixed test file

--- src/test/scala/CPUTester.scala
+++ src/test/scala/CPUTester.scala
@@ -16,20 +16,13 @@ class CPUTester(c: CPU) extends PeekPokeTester(c) {
     println("IR " + peek(c.io.IR).toInt.toHexString)
     println("PC " + peek(c.io.PC).toInt.toHexString)
     println("nzcv " + peek(c.io.nzcv).toInt.toBinaryString)
-    println("rm_imm_s " + peek(c.io.rm_imm_s))
-    println("rs_imm_s " + peek(c.io.rs_imm_s))
     println("ALU_OP " + peek(c.io.ALU_OP).toInt.toBinaryString)
-    println("Shift_OP " + peek(c.io.Shift_OP).toInt.toBinaryString)
     println("rd_s " + peek(c.io.rd_s))
     println("ALU_A_s " + peek(c.io.ALU_A_s))
     println("ALU_B_s " + peek(c.io.ALU_B_s))
-    println("Shift_out " + peek(c.io.Shift_out).toInt.toHexString)
 
     println("----------------------------------------")
     println("PC_s " + peek(c.io.PC_s).toInt.toBinaryString)
-    println("S " + peek(c.io.S))
-    println("state " + peek(c.io.state))
-    println("done " + peek(c.io.done))
 
     step(1)
 
